27031548bfdb20008bce9c21dde310802f8f3469
github username <github email address>
2020-06-26 23:09:09 -0400

    Login

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import Login from "./src/components/Login.js";
 
 function HomeScreen({ navigation }) {
   return (
@@ -38,7 +39,8 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Navigator initialRouteName="Login">
+        <Drawer.Screen name="Login" component={Login} />
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Notifications" component={NotificationsScreen} />
       </Drawer.Navigator>
