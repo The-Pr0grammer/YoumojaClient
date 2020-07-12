95c93c637f8739b2e93888f270e8ffee2f6ec36f
github username <github email address>
2020-07-11 23:58:27 -0400

    listbiz refactor/ hearts work

--- src/components/Businesses.js
+++ src/components/Businesses.js
@@ -13,10 +13,11 @@ import {
 import { Card, SearchBar, Icon } from "react-native-elements";
 import axios from "axios";
 import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
-import TextTicker from "react-native-text-ticker";
 import { connect } from "react-redux";
 const DEVICE_WIDTH = Dimensions.get("window").width;
 const DEVICE_HEIGHT = Dimensions.get("window").height;
+import ListBiz from "./ListBiz.js";
+
 class Businesses extends Component {
 	constructor(props) {
 		super(props);
@@ -90,173 +91,7 @@ class Businesses extends Component {
 					}}
 					data={this.state.businesses}
 					keyExtractor={(biz) => biz.id.toString()}
-					renderItem={({ item }) => (
-						<View style={styles.cardView}>
-							<Card
-								containerStyle={{
-									width: vw(97),
-									padding: 0,
-									borderWidth: 0,
-									shadowOffset: { height: 0, width: 0 },
-									display: "flex",
-									flexDirection: "column",
-									backgroundColor: "transparent",
-								}}
-							>
-								<Image
-									style={styles.img}
-									source={{ uri: item.business.image_url }}
-								/>
-								<View
-									style={{
-										alignSelf: "flex-end",
-										left: vw(61.3),
-										position: "absolute",
-										height: vh(30),
-										width: vh(16.5),
-										backgroundColor: "black",
-										opacity: 0.5,
-									}}
-								>
-									<TouchableOpacity
-										style={{
-											position: "absolute",
-											alignSelf: "center",
-											right: vw(12.5),
-											top: vw(3.5),
-											height: 37,
-											width: 45,
-										}}
-										onPress={() => {}}
-									>
-										<Icon name="heart" type="feather" color="red" size={37} />
-									</TouchableOpacity>
-									<Text
-										style={{
-											position: "absolute",
-											textAlign: "center",
-											fontSize: 25,
-											color: "gold",
-											fontWeight: "bold",
-											right: vw(12.5),
-											top: vh(6),
-											height: 37,
-											width: 45,
-										}}
-									>
-										1
-									</Text>
-									{/* COMMENTS ICON */}
-									<TouchableOpacity
-										style={{
-											position: "absolute",
-											alignSelf: "center",
-											right: vw(12.5),
-											top: vh(11),
-											height: 37,
-											width: 45,
-										}}
-										onPress={() => {}}
-									>
-										<Icon
-											name="chat"
-											type="materialcommunityicons"
-											color="green"
-											size={37}
-										/>
-									</TouchableOpacity>
-
-									<Text
-										style={{
-											position: "absolute",
-											textAlign: "center",
-											fontSize: 25,
-											color: "gold",
-											fontWeight: "bold",
-											right: vw(12.5),
-											top: vh(15),
-											height: 37,
-											width: 45,
-										}}
-									>
-										1
-									</Text>
-
-									<ScrollView
-										style={{
-											alignSelf: "flex-start",
-											backgroundColor: "orange",
-											width: vw(34.5),
-											height: vh(10),
-											position: "absolute",
-											top: vh(20),
-										}}
-										automaticallyAdjustInsets={false}
-										horizontal={true}
-										pagingEnabled={true}
-										scrollEnabled={true}
-										decelerationRate={0}
-										snapToAlignment={"center"}
-										snapToInterval={200}
-										scrollEventThrottle={16}
-										onScroll={(event) => {
-											var contentOffsetX = event.nativeEvent.contentOffset.x;
-											var contentOffsetY = event.nativeEvent.contentOffset.y;
-
-											var cellWidth = (DEVICE_WIDTH - 100).toFixed(2);
-											var cellHeight = (DEVICE_HEIGHT - 200).toFixed(2);
-
-											var cellIndex = Math.floor(contentOffsetX / cellWidth);
-
-											// Round to the next cell if the scrolling will stop over halfway to the next cell.
-											if (
-												contentOffsetX -
-													Math.floor(contentOffsetX / cellWidth) * cellWidth >
-												cellWidth
-											) {
-												cellIndex++;
-											}
-
-											// Adjust stopping point to exact beginning of cell.
-											contentOffsetX = cellIndex * cellWidth;
-											contentOffsetY = cellIndex * cellHeight;
-
-											event.nativeEvent.contentOffsetX = contentOffsetX;
-											event.nativeEvent.contentOffsetY = contentOffsetY;
-
-											// this.setState({contentOffsetX:contentOffsetX,contentOffsetY:contentOffsetY});
-											console.log("cellIndex:" + cellIndex);
-
-											console.log("contentOffsetX:" + contentOffsetX);
-											// contentOffset={{x:this.state.contentOffsetX,y:0}}
-										}}
-									>
-										<Image
-											source={require("../images/LOGO.png")}
-											style={styles.image}
-										/><Image
-										source={require("../images/LOGO.png")}
-										style={styles.image}
-									/>
-									</ScrollView>
-								</View>
-								{/* BADGES VIEW*/}
-							</Card>
-							<View styles={{ width: vw(100), backgroundColor: "red" }}>
-								{/* <TextTicker
-									duration={Math.random * 40000}
-									loop
-									bounce
-									repeatSpacer={0}
-									marqueeDelay={Math.random() * 2000}
-									style={styles.cardText}
-								>
-									made random text cos this is just an example of a businesses
-									description !!!!!!! yeeeeeeeeeeee 🔥🤯♥️
-								</TextTicker> */}
-							</View>
-						</View>
-					)}
+					renderItem={({ item }) => <ListBiz biz={item} />}
 				/>
 			</View>
 		);
@@ -282,20 +117,6 @@ const styles = StyleSheet.create({
 		height: "80%",
 		width: "100%",
 		top: vh(2),
-	},
-	cardView: {
-		marginTop: vh(-0.5),
-		padding: 0,
-		display: "flex",
-		flexDirection: "row",
-	},
-	img: {
-		flex: 2,
-		marginLeft: vh(-1.7),
-		width: vw(65),
-		height: vh(30),
-		opacity: 1.0,
-		borderRadius: 2,
 	},
 	cardText: {
 		width: vw(100),
---
+++ src/components/ListBiz.js
@@ -0,0 +1,244 @@
+import React, { Component } from "react";
+import axios from "axios";
+import {
+	ScrollView,
+	View,
+	Text,
+	Image,
+	StyleSheet,
+	TouchableOpacity,
+	Dimensions,
+} from "react-native";
+import { Card, SearchBar, Icon } from "react-native-elements";
+import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
+import TextTicker from "react-native-text-ticker";
+import { connect } from "react-redux";
+const DEVICE_WIDTH = Dimensions.get("window").width;
+const DEVICE_HEIGHT = Dimensions.get("window").height;
+
+class ListBiz extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hearts: 0,
+			page: 1,
+			error: null,
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ hearts: this.props.biz.hearts });
+	}
+
+	incHearts = () => {
+		const axios = require("axios");
+		this.setState((prevState) => ({ hearts: prevState.current + 1 }));
+		axios
+			.patch(
+				`http://localhost:3000/businesses/${this.props.biz.business.id}`,
+				{
+					hearts: this.state.hearts + 1,
+				},
+				{ headers: { "Content-Type": "application/json" } }
+			)
+			.then(function (response) {
+				// console.log(response);
+			});
+		// .catch((error) => {
+		// 	console.log(error.response);
+		// });
+	};
+
+	render() {
+		console.log(this.props.biz.hearts);
+		return (
+			<View style={styles.cardView}>
+				<Card
+					containerStyle={{
+						width: vw(97),
+						padding: 0,
+						borderWidth: 0,
+						shadowOffset: { height: 0, width: 0 },
+						display: "flex",
+						flexDirection: "column",
+						backgroundColor: "transparent",
+					}}
+				>
+					<Image
+						style={styles.img}
+						source={{ uri: this.props.biz.business.image_url }}
+					/>
+					<View
+						style={{
+							alignSelf: "flex-end",
+							left: vw(61.3),
+							position: "absolute",
+							height: vh(30),
+							width: vh(16.5),
+							backgroundColor: "black",
+							opacity: 0.5,
+						}}
+					>
+						<TouchableOpacity
+							style={{
+								position: "absolute",
+								alignSelf: "center",
+								right: vw(12.5),
+								top: vw(3.5),
+								height: 37,
+								width: 45,
+							}}
+							onPress={() => {
+								this.incHearts();
+							}}
+						>
+							<Icon name="heart" type="feather" color="red" size={37} />
+						</TouchableOpacity>
+						<Text
+							style={{
+								position: "absolute",
+								textAlign: "center",
+								fontSize: 25,
+								color: "gold",
+								fontWeight: "bold",
+								right: vw(12.5),
+								top: vh(6),
+								height: 37,
+								width: 45,
+							}}
+						>
+							1
+						</Text>
+						{/* COMMENTS ICON */}
+						<TouchableOpacity
+							style={{
+								position: "absolute",
+								alignSelf: "center",
+								right: vw(12.5),
+								top: vh(11),
+								height: 37,
+								width: 45,
+							}}
+							onPress={() => {}}
+						>
+							<Icon
+								name="chat"
+								type="materialcommunityicons"
+								color="green"
+								size={37}
+							/>
+						</TouchableOpacity>
+
+						<Text
+							style={{
+								position: "absolute",
+								textAlign: "center",
+								fontSize: 25,
+								color: "gold",
+								fontWeight: "bold",
+								right: vw(12.5),
+								top: vh(15),
+								height: 37,
+								width: 45,
+							}}
+						>
+							{this.state.hearts}
+						</Text>
+
+						<ScrollView
+							style={{
+								alignSelf: "flex-start",
+								backgroundColor: "orange",
+								width: vw(34.5),
+								height: vh(10),
+								position: "absolute",
+								top: vh(20),
+							}}
+							automaticallyAdjustInsets={false}
+							horizontal={true}
+							pagingEnabled={true}
+							scrollEnabled={true}
+							decelerationRate={0}
+							snapToAlignment={"center"}
+							snapToInterval={200}
+							scrollEventThrottle={16}
+							onScroll={(event) => {
+								var contentOffsetX = event.nativeEvent.contentOffset.x;
+								var contentOffsetY = event.nativeEvent.contentOffset.y;
+
+								var cellWidth = (DEVICE_WIDTH - 100).toFixed(2);
+								var cellHeight = (DEVICE_HEIGHT - 200).toFixed(2);
+
+								var cellIndex = Math.floor(contentOffsetX / cellWidth);
+
+								// Round to the next cell if the scrolling will stop over halfway to the next cell.
+								if (
+									contentOffsetX -
+										Math.floor(contentOffsetX / cellWidth) * cellWidth >
+									cellWidth
+								) {
+									cellIndex++;
+								}
+
+								// Adjust stopping point to exact beginning of cell.
+								contentOffsetX = cellIndex * cellWidth;
+								contentOffsetY = cellIndex * cellHeight;
+
+								event.nativeEvent.contentOffsetX = contentOffsetX;
+								event.nativeEvent.contentOffsetY = contentOffsetY;
+
+								// this.setState({contentOffsetX:contentOffsetX,contentOffsetY:contentOffsetY});
+								console.log("cellIndex:" + cellIndex);
+
+								console.log("contentOffsetX:" + contentOffsetX);
+								// contentOffset={{x:this.state.contentOffsetX,y:0}}
+							}}
+						>
+							<Image
+								source={require("../images/LOGO.png")}
+								style={styles.badge}
+							/>
+							<Image
+								source={require("../images/LOGO.png")}
+								style={styles.badge}
+							/>
+						</ScrollView>
+					</View>
+					{/* BADGES VIEW*/}
+				</Card>
+				<View styles={{ width: vw(100), backgroundColor: "red" }}>
+					{/* <TextTicker
+                duration={Math.random * 40000}
+                loop
+                bounce
+                repeatSpacer={0}
+                marqueeDelay={Math.random() * 2000}
+                style={styles.cardText}
+            >
+                made random text cos this is just an example of a businesses
+                description !!!!!!! yeeeeeeeeeeee 🔥🤯♥️
+            </TextTicker> */}
+				</View>
+			</View>
+		);
+	}
+}
+
+export default ListBiz;
+
+const styles = StyleSheet.create({
+	cardView: {
+		marginTop: vh(-0.5),
+		padding: 0,
+		display: "flex",
+		flexDirection: "row",
+	},
+	img: {
+		flex: 2,
+		marginLeft: vh(-1.7),
+		width: vw(65),
+		height: vh(30),
+		opacity: 1.0,
+		borderRadius: 2,
+	},
+});
